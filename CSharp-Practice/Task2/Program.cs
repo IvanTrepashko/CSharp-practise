c3f7b0aa831182956062dabbdbaf2237c1356e64
Danila Baidak <danila.fulov@gmail.com>
2020-03-10 21:32:05 +0300

    Task 2 done

---
+++ CSharp-Practice/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            // тип параметра - object 
+            arrayList.Add(45); // true,boxing
+            arrayList.Add(true); // true, boxing 
+            arrayList.Add("Hello!");// false
+            arrayList.Add(23.45); //true, boxing
+
+
+        }
+    }
+}
