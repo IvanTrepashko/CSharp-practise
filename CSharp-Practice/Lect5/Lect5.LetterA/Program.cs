d5341bf0ce2289ad413dc9e60ecb9b3c91001cb8
Danila Baidak <danila.fulov@gmail.com>
2020-03-17 21:32:36 +0300

    Task 2 done

---
+++ CSharp-Practice/Lect5/Lect5.LetterA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lect5.LetterA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int count = 0;
+            str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Count of letter 'a' is "+count);
+        }
+    }
+}
