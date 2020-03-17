a80a4552aa8cb18ef9c5f7ce49c61f7fb77bdb9a
Danila Baidak <danila.fulov@gmail.com>
2020-03-17 21:29:15 +0300

    Task 1 done

---
+++ CSharp-Practice/Lect5/Lect5.Greeting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lect5.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan time = new TimeSpan();
+            time = DateTime.Now.TimeOfDay;
+            if (time.Hours >= 9 && time.Hours <= 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (time.Hours > 12 && time.Hours <= 15)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else if (time.Hours > 15 && time.Hours <= 22)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+        }
+    }
+}
