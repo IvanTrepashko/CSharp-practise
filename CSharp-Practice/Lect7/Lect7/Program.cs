e8ecc9f72ac9bcaecf504b6232a620b106c866eb
Danila Baidak <danila.fulov@gmail.com>
2020-03-24 19:59:48 +0300

    Task 2 done

---
+++ CSharp-Practice/Lect7/Lect7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lect7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task_2();
+        }
+
+        static  void task_2()
+        {
+            object[] arr = new object[3];
+            arr[0] = 23;
+            arr[1] = 'B';
+            arr[2] = "Hello";
+            foreach(object item in arr)
+                Console.WriteLine(item);
+            arr[0] = (int)(arr[0]) + 10;
+            arr[2] += ", guys";
+            foreach (object item in arr)
+                Console.WriteLine(item);
+        }
+    }
+}
