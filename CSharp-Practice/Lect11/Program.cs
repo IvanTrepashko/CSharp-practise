936861e239d989569aaf828cff77f61b8ab7313d
IvanTrepashko <goodfellawk@gmail.com>
2020-04-14 21:50:28 +0300

    Done

---
+++ CSharp-Practice/Lect11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lect11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[3];
+
+            Circle circle = new Circle();
+            Square square = new Square();
+            Triangle triangle = new Triangle();
+
+            shapes[0] = circle;
+            shapes[1] = square;
+            shapes[2] = triangle;
+
+            foreach(Shape shape in shapes)
+            {
+                shape.CalculateSquare();
+                shape.ShowInformation();
+            }
+        }
+    }
+}
---
+++ CSharp-Practice/Lect11/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect11
+{
+    class Triangle : Shape
+    {
+        public int Side { get; set; }
+        public int Height { get; set; }
+
+        public override void CalculateSquare()
+        {
+            Square = 0.5 * Side * Height;
+        }
+
+        public override void ShowInformation()
+        {
+            Console.WriteLine($"This is a Triangle. CLR Type is {this.GetType()}. Square is {Square}.");
+        }
+
+        public Triangle()
+        {
+            Random rnd = new Random();
+            Side = rnd.Next(1, 10);
+            Height = rnd.Next(1, 10);
+        }
+    }
+}
