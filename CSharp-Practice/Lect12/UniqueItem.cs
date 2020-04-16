574b216533e4a98c6460f3406d1efd7b36128036
IvanTrepashko <goodfellawk@gmail.com>
2020-04-16 20:24:57 +0300

    Done

---
+++ CSharp-Practice/Lect12/UniqueItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect12
+{
+    class UniqueItem
+    {
+        public static int TotalId { get; set; } = 0;
+        public int Id { get; set; }
+        public UniqueItem()
+        {
+            Id = ++TotalId;
+        }
+        public static void SetStartID(int x)
+        {
+            TotalId = x;
+        }
+    }
+}
