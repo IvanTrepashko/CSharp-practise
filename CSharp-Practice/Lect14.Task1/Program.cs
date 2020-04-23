3890865d79b80db6008fbba43aa77d179f5d50fb
IvanTrepashko <goodfellawk@gmail.com>
2020-04-23 19:48:20 +0300

    Task 1 done

---
+++ CSharp-Practice/Lect14.Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lect14.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello, world";
+            Console.WriteLine(str.TrimString());
+        }
+    }
+}
---
+++ CSharp-Practice/Lect14.Task1/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect14.Task1
+{
+    public static class StringExtensions
+    {
+        public static string TrimString(this string str)
+        {
+            return str.Substring(0, 5) + "...";
+        }
+    }
+}
